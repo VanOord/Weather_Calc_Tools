db22ec2b68ac65c844bc5b31ae034f93dc51fd0a
AlekVO <133217040+AlekVO@users.noreply.github.com>
2023-06-02 11:06:35 +0200

    Added original files via upload

---
+++ Package_Installer_WW_GUI_CSV_Save.py
@@ -0,0 +1,28 @@
+
+import subprocess
+
+def install_package(package):
+    try:
+        subprocess.check_call(['pip', 'install', '--upgrade', package])
+        print(f"Successfully installed {package}")
+    except subprocess.CalledProcessError:
+        print(f"Error installing {package}")
+
+def main():
+
+    # Install or update packages
+    packages = [
+        'pandas',
+        'matplotlib',
+        'numpy',
+        'calendar',
+        'tabulate',
+        'PySimpleGUI',
+        'os'
+    ]
+
+    for package in packages:
+        install_package(package)
+
+if __name__ == '__main__':
+    main()
